042574ccdd60ecf31cba1abcbf9663f00a232066
ichmen <ichmen@rambler.ru>
2022-07-25 20:16:42 +0300

    l6t1

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: { filename: "bundle.js" },
+  module: {
+    rules: [
+      { test: /.s?css$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1024,
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
