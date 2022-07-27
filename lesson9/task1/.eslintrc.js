0377c5ce5b9a6367e97a4f33e8109ab06a4b6e0b
ichmen <ichmen@rambler.ru>
2022-07-27 17:24:36 +0300

    l8t1

---
+++ lesson9/task1/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint-config-airbnb-base",
+  rules: {
+    "linebreak-style": ["error", "windows"],
+  },
+};
